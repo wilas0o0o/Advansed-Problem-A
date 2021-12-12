bfd52e2da8cc960d1393420eaa524cdb30dd71f2
wilas0o0o <03mcu30@gmail.com>
2021-12-12 12:39:30 +0000

    [Add]Relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,13 @@ class User < ApplicationRecord
   has_many :favorites,dependent: :destroy
   has_many :book_comments,dependent: :destroy
 
+  has_many :active_relationships,class_name: "Relationship",foregin_key: "follower_id",dependent: :destroy
+  has_many :passive_relationships,class_name: "Relationship",foregin_key: "followed_id",dependent: :destroy
+
+  has_many :followings,through: :active_relationships,source: :followed
+  has_many :followers,through: :passive_relationships,source: :follower
+  
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books do 
+  resources :users,only: [:show,:index,:edit,:update] do
+    resources :relationships,only: [:create,:destroy]  
+  end
+  resources :books do
     resource :favorites, only: [:create,:destroy]
     resources :book_comments, only: [:create,:destroy]
   end
