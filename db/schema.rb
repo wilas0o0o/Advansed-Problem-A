2d336a3f14c270149c8792cf9fb7e1a86ef3ee18
wilas0o0o <03mcu30@gmail.com>
2021-12-12 04:55:27 +0000

    [Start]応用課題6-2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_12_022356) do
+ActiveRecord::Schema.define(version: 2021_12_12_045422) do
+
+  create_table "book_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "book_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
