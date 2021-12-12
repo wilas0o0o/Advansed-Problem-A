69f55ec2ff2d6ba961131504e7bc4d29d2843b27
wilas0o0o <03mcu30@gmail.com>
2021-12-12 12:48:07 +0000

    [Update]Routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   resources :users,only: [:show,:index,:edit,:update] do
     resources :relationships,only: [:create,:destroy]
+    get "followings" => "relationships#followings",as: "followings"
+    get "followers" => "relationships#followers",as: "followers"
   end
   resources :books do
     resource :favorites, only: [:create,:destroy]
