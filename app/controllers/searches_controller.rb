be557162560aa4ca9bb65e201f78e4f228f421ec
wilas0o0o <03mcu30@gmail.com>
2021-12-13 02:33:12 +0000

    [Add]SearchAcion

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,14 @@
 class SearchesController < ApplicationController
+  before_action :authenticate_user!
+  
+  def search
+    @model = params["model"]
+    @method = params["method"]
+    @content = params["content"]
+    if @model == "user"
+      @records = User.search_for(@method,@content)
+    elsif @model = "book"
+      @records = Book.searbh_for(@method,@content)
+    end
+  end
 end
